8915c56231f624cebde5417be6b3c05d610aeed0
Jash Pandya <pandyajash2005@gmail.com>
2025-12-08 19:51:44 +0530

    Refactor romanToInt method to use a static map
    T.C. : O(1) & S.C. : O(1) . 
    
    Since each input has a static value and which is stored in a static hashmap - values.

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,39 @@
 class Solution {
+    static Map<String, Integer> values = new HashMap<>();
+
+    static{
+        values.put("I" , 1);
+        values.put("V" , 5);
+        values.put("X" , 10);
+        values.put("L" , 50);
+        values.put("C" , 100);
+        values.put("D" , 500);
+        values.put("M" , 1000);
+        values.put("IV" , 4);
+        values.put("IX" , 9);
+        values.put("XL" , 40);
+        values.put("XC", 90);
+        values.put("CD" , 400);
+        values.put("CM" , 900);
+    }
+
     public int romanToInt(String s) {
-        int res = 0;
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
+           int sum = 0, i = 0;
 
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
-                res -= roman.get(s.charAt(i));
-            } else {
-                res += roman.get(s.charAt(i));
+           while(i < s.length()){
+            if(i < s.length() - 1){
+                String twoValues = s.substring(i, i+2);
+                if(values.containsKey(twoValues)){
+                    sum += values.get(twoValues);
+                    i += 2;
+                    continue;
+                }
             }
-        }
+            String oneValue = s.substring(i, i+1);
+                sum += values.get(oneValue);
+                i++;
+           }
 
-        return res + roman.get(s.charAt(s.length() - 1));        
+           return sum;
     }
 }
