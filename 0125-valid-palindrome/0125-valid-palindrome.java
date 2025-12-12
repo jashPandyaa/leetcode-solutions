0849ce64d92e83a9781b910f9d708b3e1ee1c358
Jash Pandya <pandyajash2005@gmail.com>
2025-12-12 19:32:27 +0530

    Time: 131 ms (5.07%), Space: 48.7 MB (5.21%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        String sR = "";
+
+        for(int i=s.length()-1; i>=0; i--){
+            sR += s.charAt(i);
+        }
+
+        if(!sR.equals(s)){  //compares actual content for strings
+             return false;
+        }
+
+        return true;
+    }
+}
+//        replaceAll() → Replaces all matching characters.
+//
+//        "[^a-z0-9]" → The magic pattern inside:
+//
+//        [ ] → Match any character inside these brackets.
+//
+//        ^ → Inside [ ], it means "NOT" (so [^abc] means "not a, b, or c").
+//
+//        a-z → All lowercase letters (since we already converted to lowercase).
+//
+//        0-9 → All digits.
+//
+//        So [^a-z0-9] → "Match any character that is NOT a letter or number."
+//
+//        "" → Replace those unwanted characters with nothing (delete them).
