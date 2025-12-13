0cb34bd022ddf110aa2809a6b6c563fefd7e7fd5
Jash Pandya <pandyajash2005@gmail.com>
2025-12-13 18:32:31 +0530

    Time: 5 ms (77.36%), Space: 76.9 MB (64.81%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int maxArea(int[] height) {
+    //2 Pointer approach
+    int maxWater = 0;
+    int lp = 0;
+    int rp = height.length-1;
+
+    while(lp != rp){
+        int ht = Math.min(height[lp] , height[rp]);
+        int width = rp - lp;
+        int currWater = ht * width;
+        maxWater = Math.max(currWater, maxWater);
+
+        //pointer updatation
+        if( height[lp] < height[rp] ){
+            lp++;
+        }else{
+            rp--;
+        }
+    }
+
+    return maxWater;
+    }
+}
