058f5c41ccbd38e4b9e80aab5362a0f7d3c2f317
Pandya Jashkumar Atulkumar <pandyajash2005@gmail.com>
2026-01-07 18:59:30 +0530

    Time: 0 ms (100%), Space: 45.4 MB (24.05%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public Integer prev;
+    public boolean isValidBST(TreeNode root) {
+        prev = null;
+
+        return inOrder(root);
+    }
+
+    private boolean inOrder(TreeNode root){
+        if(root == null) return true;
+
+        if(!inOrder(root.left)){
+            return false;
+        }
+         
+        if(prev != null && root.val <= prev){
+            return false;
+        }
+
+        prev = root.val;
+
+        return inOrder(root.right);
+    }
+}
