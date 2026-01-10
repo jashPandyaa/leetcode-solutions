b7c6800a5c95b94635b5c276bd8500150cc3bdc7
Jashkumar Pandya <pandyajash2005@gmail.com>
2026-01-10 15:06:59 +0530

    Time: 0 ms (100%), Space: 46.7 MB (59.99%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxGain(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        int priceNewPath = node.val + leftGain + rightGain;
+
+        maxSum = Math.max(maxSum , priceNewPath);
+
+        return node.val+Math.max(leftGain, rightGain);
+    }  
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+}
