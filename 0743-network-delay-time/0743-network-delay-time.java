5d2e91952579357352c7515745b5c05a122af52f
Jashkumar Pandya <pandyajash2005@gmail.com>
2026-01-19 19:47:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        //create a graph represented as an adjacency list
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for(int []edge : times){
+            graph.cmoputeIfAbsent(edge[0], x -> new ArrayList<>()).add(new int[]{edge[1] , edge[2]});
+        } 
+
+        //use a priorityQueue to select the node with the minimum distance
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.offer(new int[]{k , 0}); //start from node k with distance of 0
+
+        //Initialize distances array with infinity
+        int[] distances = new int[n + 1];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        distances[k] = 0;   //Distance to itself is zero
+
+        while(!pq.isEmpty()){
+            int[] current = pq.poll();
+            int currentNode = current[0];
+            int currentDist = current[1];
+
+            //if we already found a shorter path before than continue
+            if(currentDist > distances[currentNode]){
+                continue;
+            }
+
+            //update the distances of neighbouring nodes
+            if(graph.containsKey(currentNode)){
+                for(int[] neighbour : graph.get(currentNode)){
+                    int nextNode = neighbour[0];
+                    int nextDist = currentDist + neighbour[1];
+
+                    if(nextDist < distances[nextNode]){
+                        distances[nextNode] = nextDist;
+                        pq.offer(new int[]{nextNode, nextDist});
+                    }
+                }
+            }
+        }
+
+        //Find the maximum distance from the source node to all other nodes
+        int maxDist = Arrays.stream(distances).skip(1).max().getAsInt();
+        return maxDist == Integer.MAX_VALUE ? -1 : maxDist;
+    }
+}
