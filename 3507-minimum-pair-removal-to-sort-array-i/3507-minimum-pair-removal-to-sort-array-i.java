61e8f9643407c650236375355fefd23fdb25cb4e
Jashkumar Pandya <pandyajash2005@gmail.com>
2026-01-22 19:15:07 +0530

    Time: 1 ms (100%), Space: 44.2 MB (90.73%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.java
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.java
@@ -1,42 +1,41 @@
 class Solution {
     public int minimumPairRemoval(int[] nums) {
-        //BruteForce : Time comp - O(n ^ 3)
-        List<Integer> list = new ArrayList<>();
-        for (int num : nums) {
-            list.add(num);
-        }
-        
+        int n = nums.length;
         int operations = 0;
         
-        while (!isSorted(list)) {
-            // Find the adjacent pair with minimum sum
-            int minSum = Integer.MAX_VALUE;
-            int minIndex = 0;
+        // Keep merging until sorted
+        while (true) {
+            // Check if already sorted
+            boolean sorted = true;
+            for (int i = 0; i < n - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    sorted = false;
+                    break;
+                }
+            }
+            if (sorted) break;
             
-            for (int i = 0; i < list.size() - 1; i++) {
-                int sum = list.get(i) + list.get(i + 1);
+            // Find minimum sum pair
+            int minSum = Integer.MAX_VALUE;
+            int minIdx = -1;
+            
+            for (int i = 0; i < n - 1; i++) {
+                int sum = nums[i] + nums[i + 1];
                 if (sum < minSum) {
                     minSum = sum;
-                    minIndex = i;
+                    minIdx = i;
                 }
             }
             
-            // Replace the pair with their sum
-            list.set(minIndex, minSum);
-            list.remove(minIndex + 1);
-            
+            // Merge the pair: replace nums[minIdx] with sum, shift rest left
+            nums[minIdx] = minSum;
+            for (int i = minIdx + 1; i < n - 1; i++) {
+                nums[i] = nums[i + 1];
+            }
+            n--; // Reduce effective array size
             operations++;
         }
         
         return operations;
-    }
-    
-    private boolean isSorted(List<Integer> list) {
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i) > list.get(i + 1)) {
-                return false;
-            }
-        }
-        return true;
     }
 }
