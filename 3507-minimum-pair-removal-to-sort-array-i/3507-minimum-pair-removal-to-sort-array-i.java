b8e483ae96e0ab2272bd4550d7e99e285572192f
Jashkumar Pandya <pandyajash2005@gmail.com>
2026-01-22 19:07:43 +0530

    Time: 3 ms (66.06%), Space: 44.6 MB (32.51%) - LeetHub

---
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        
+        int operations = 0;
+        
+        while (!isSorted(list)) {
+            // Find the adjacent pair with minimum sum
+            int minSum = Integer.MAX_VALUE;
+            int minIndex = 0;
+            
+            for (int i = 0; i < list.size() - 1; i++) {
+                int sum = list.get(i) + list.get(i + 1);
+                if (sum < minSum) {
+                    minSum = sum;
+                    minIndex = i;
+                }
+            }
+            
+            // Replace the pair with their sum
+            list.set(minIndex, minSum);
+            list.remove(minIndex + 1);
+            
+            operations++;
+        }
+        
+        return operations;
+    }
+    
+    private boolean isSorted(List<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
